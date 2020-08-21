fd81710fec69f9ee85fb80decc6e9c1a789db2fa
Ian Choi <achiinto@gmail.com>
2020-08-21 23:53:33 +0800

    Change Profile to use Port or 5000

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
@@ -10,5 +11,5 @@ manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
