81d8adb07522059ad22733037e125aa66ef1e7af
Ian Choi <achiinto@gmail.com>
2020-08-21 23:21:36 +0800

    Correction to port for heroku

--- manage.py
+++ manage.py
@@ -11,4 +11,4 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, host='0.0.0.0', port=5000)
